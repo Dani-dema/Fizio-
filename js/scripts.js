82be2663e4a803f188df3bba549253081e0324a7
Ramadan Dema <danidema95@gmail.com>
2020-08-29 12:35:17 +0200

    commit 2

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#mycarousel').carousel({interval:1500});
+    
+    $('#rezervoButton').click(function(){
+        $('#myModal').modal('show');
+    })
+    
+        
+});
